11d8ebc591b9debdedb2082110114873d1695f5e
abiodun.sabitu <abiodun.sabitu@sterling.ng>
2024-01-28 19:17:44 +0100

    exercise 08: solve palindromes checker and pass all 8 tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let regex = /[^A-Za-z0-9]/g;
+  let cleanedString = str.replace(regex, "").toLowerCase();
+  const result = cleanedString.split("").reverse().join("");
+  return result === cleanedString;
 };
 
 // Do not edit below this line
